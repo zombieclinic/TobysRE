341680d9f6e6e46d9cbe9973f314851416877b72
raymn <raymnd10@gmail.com>
2026-03-01 23:55:50 -0500

    side scrolling game has controls

--- Assets/scripts/blueCardKeyGame/startBlueGame.cs
+++ Assets/scripts/blueCardKeyGame/startBlueGame.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class startBlueGame : MonoBehaviour
 {
-   
+   private PlayerMotorTopDown topControls;
+    private playerSideWalk sideControls;
+    public float gravity = 3;
 
     void Start()
     {
          GameObject player = GameObject.FindWithTag("Player");
 
             Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-            rb.gravityScale = 11f;
+            rb.gravityScale = gravity;
             player.transform.position = new Vector3(-7.4f,3.8f, 0f);
-            PlayerBrain brain = player.GetComponent<PlayerBrain>();
-             brain.ReStartPlayerControls();
-        
+            
+
+              
+        topControls = player.GetComponent<PlayerMotorTopDown>();
+        topControls.enabled = false;
+
+        sideControls = player.GetComponent<playerSideWalk>();
+        sideControls.enabled = true;
+
+        PlayerBrain brain = player.GetComponent<PlayerBrain>();
+            brain.ReStartPlayerControls();
     }
 
     // Update is called once per frame
--- Assets/scripts/player/playerSideWalk.cs
+++ Assets/scripts/player/playerSideWalk.cs
@@ -2,17 +2,76 @@ using UnityEngine;
 
 public class playerSideWalk : MonoBehaviour
 {
-    
+    [Header("Speed")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 10f;
 
-    
-    void Start()
+    [Header("Stamina")]
+    [SerializeField] private float maxStamina = 100f;
+    [SerializeField] private float staminaDrainPerSecond = 15f;
+    [SerializeField] private float staminaGainPerSecond = 10f;
+
+    [Header("Debug (Read Only)")]
+    [SerializeField] private float stamina; 
+    public float StaminaRaw => stamina;
+
+    public int facingDirection = 1;
+
+    private Rigidbody2D rb;
+    private PlayerBrain brain;
+    private PlayerNoise playerNoise;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        brain = GetComponent<PlayerBrain>();
+        playerNoise = GetComponent<PlayerNoise>();
+        stamina = maxStamina;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
+       
+        rb.linearVelocity = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+            if (brain == null) return;
+
+        float moveX = brain.MoveInput.x;
+        bool moving = Mathf.Abs(moveX) > 0.01f;
+
+        // Can sprint only if holding sprint, moving, and have stamina
+        bool canSprint = brain.SprintHeld && moving && stamina > 5f;
+
+        // Drain/regen stamina
+        if (canSprint)
+            stamina -= staminaDrainPerSecond * Time.fixedDeltaTime;
+        else
+            stamina += staminaGainPerSecond * Time.fixedDeltaTime;
+
+        stamina = Mathf.Clamp(stamina, 0f, maxStamina);
+
+        // Speed based on stamina-allowed sprint
+        float speed = canSprint ? runSpeed : walkSpeed;
+       
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+
+        // Noise
+        if (playerNoise != null && moving)
+            playerNoise.AddMovementNoise(canSprint, Time.fixedDeltaTime);
+
         
+        if (brain.MoveInput.x > 0.01f && transform.localScale.x < 0f) Flip();
+        else if (brain.MoveInput.x < -0.01f && transform.localScale.x > 0f) Flip();
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        Vector3 s = transform.localScale;
+        s.x *= -1;
+        transform.localScale = s;
     }
 }

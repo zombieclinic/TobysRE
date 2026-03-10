da23fbff0f553695fa1621fb3fdf595226ccdc21
raymn <raymnd10@gmail.com>
2026-03-10 17:24:56 -0400

    added sound controls

--- Assets/scripts/UI/pauseScreen.cs
+++ Assets/scripts/UI/pauseScreen.cs
@@ -2,39 +2,92 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections;
 
-public class pauseScreen : MonoBehaviour
+public class PauseScreen : MonoBehaviour
 {
-
     private UIDocument uiDocument;
-
-
     private Button exitButton;
+    private Slider masterSlider;
+    private Slider SFXSlider;
+    private Slider MusicSlider;
 
-    void OnEnable()
+    [SerializeField] private SoundMixerManager mixer;
+
+    private void Awake()
     {
         uiDocument = GetComponent<UIDocument>();
-        var root = uiDocument.rootVisualElement;
-       
-        exitButton = root.Q<Button>("exitLabel");
-
-        exitButton.clicked -= OnExitClicked;
-        exitButton.clicked += OnExitClicked;
-
-        
     }
 
+    private void OnEnable()
+    {
+        var root = uiDocument.rootVisualElement;
 
+        exitButton = root.Q<Button>("exitLabel");
+        masterSlider = root.Q<Slider>("MasterVolumeSlider");
+        SFXSlider = root.Q<Slider>("SFXVolumeSlider");
+        MusicSlider = root.Q<Slider>("MusicVolumeSlider");
+
+        if (exitButton != null)
+        {
+            exitButton.clicked -= OnExitClicked;
+            exitButton.clicked += OnExitClicked;
+        }
+
+        if (masterSlider != null)
+        {
+            masterSlider.UnregisterValueChangedCallback(OnMasterVolumeChanged);
+            masterSlider.RegisterValueChangedCallback(OnMasterVolumeChanged);
+        }
+
+        if (SFXSlider != null)
+        {
+            SFXSlider.UnregisterValueChangedCallback(OnSFXVolumeChanged);
+            SFXSlider.RegisterValueChangedCallback(OnSFXVolumeChanged);
+        }
+        if (MusicSlider != null)
+        {
+            MusicSlider.UnregisterValueChangedCallback(OnMusicChanged);
+            MusicSlider.RegisterValueChangedCallback(OnMusicChanged);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (exitButton != null)
+            exitButton.clicked -= OnExitClicked;
+
+        if (masterSlider != null)
+            masterSlider.UnregisterValueChangedCallback(OnMasterVolumeChanged);
+
+        if (SFXSlider != null)
+            SFXSlider.UnregisterValueChangedCallback(OnMasterVolumeChanged);
+        
+        if (MusicSlider != null)
+            MusicSlider.UnregisterValueChangedCallback(OnMusicChanged);
+    }
+
+    private void OnMasterVolumeChanged(ChangeEvent<float> evt)
+    {
+        mixer.SetMasterVolume(evt.newValue);
+    }
+
+    private void OnSFXVolumeChanged(ChangeEvent<float> evt)
+    {
+        mixer.SetSoundFXVolume(evt.newValue);
+    }
+    
+    private void OnMusicChanged(ChangeEvent<float> evt)
+    {
+        mixer.SetMusicVolume(evt.newValue);
+    }
 
     private void OnExitClicked()
     {
-       
         StartCoroutine(QuitAfterDelay());
-       
     }
 
-        private IEnumerator QuitAfterDelay()
+    private IEnumerator QuitAfterDelay()
     {
-        yield return new WaitForSecondsRealtime(1f); // works even when timeScale = 0
+        yield return new WaitForSecondsRealtime(1f);
         LoadQuit();
     }
 
@@ -47,5 +100,4 @@ public class pauseScreen : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
     #endif
     }
-
 }
